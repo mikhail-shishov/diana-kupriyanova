c1966c03198df8c554b7986053c1cdc21abc01f9
Mikhail Shishov <shishovmike96@gmail.com>
2026-04-01 19:06:24 +0200

    classes

---
+++ stul_FIX/php/classes/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'db.php';
+
+class Article {
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function create($user_id, $title, $content) {
+        $sql = "INSERT INTO articles (user_id, title, content) VALUES (:user_id, :title, :content)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            'user_id' => $user_id,
+            'title' => $title,
+            'content' => $content
+        ]);
+    }
+}
---
+++ stul_FIX/php/classes/User.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'db.php';
+
+class User {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function register($email, $password, $first_name, $last_name, $tel) {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (email, password, first_name, last_name, tel) VALUES (:email, :password, :first_name, :last_name. :tel)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            'email' => $email,
+            'password' => $hashed_password,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'tel' => $tel
+        ]);
+    }
+
+    public function login($email, $password) {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $user = $stmt->fetch();
+
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+//CREATE TABLE articles (
+//    id INT AUTO_INCREMENT PRIMARY KEY,
+//      user_id INT NOT NULL,
+//      title VARCHAR(255) NOT NULL,
+//      content TEXT NOT NULL,
+//      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+//)
